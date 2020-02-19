5e3b0e9535e7c93ee1e747693125792a15f2838d
liliarsis <xlilia.ri@gmail.com>
2020-02-19 00:33:05 -0800

    practica1 changes

--- practicas_tareas/practica1.scala
+++ practicas_tareas/practica1.scala
@@ -1,11 +1,15 @@
 // Assessment 1/Practica 1
 //1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
 import scala.math.Pi
-var c = 15
-(c)/(2*Pi) 
+var circunferencia = 5
+(circunferencia)/(2*Pi) 
 //2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+
 //3. Dada la variable bird = "tweet", utiliza interpolacion de string para
 //   imprimir "Estoy ecribiendo un tweet"
+var bird = "tweet"
+printf("Estoy escrbiendo un " + bird)
+
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
 //   secuencia "Luke"
 //5. Cual es la diferencia en value y una variable en scala?
