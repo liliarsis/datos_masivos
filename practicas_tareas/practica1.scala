dc6d44cc395d142a30f8c155f0de0ba9c8d29fc6
liliarsis <xlilia.ri@gmail.com>
2020-02-20 12:09:50 -0800

    exercises answers added

--- practicas_tareas/practica1.scala
+++ practicas_tareas/practica1.scala
@@ -1,16 +1,32 @@
 // Assessment 1/Practica 1
 //1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
 import scala.math.Pi
-var circunferencia = 5
+import scala.math.sqrt
+
+//teniendo en cuenta la circunferencia
+var circunferencia = 5 
 (circunferencia)/(2*Pi) 
+//teniendo en cuenta el area
+var area = 8 
+sqrt(area/Pi) 
+
 //2. Desarrollar un algoritmo en scala que me diga si un numero es primo
 
 //3. Dada la variable bird = "tweet", utiliza interpolacion de string para
 //   imprimir "Estoy ecribiendo un tweet"
-var bird = "tweet"
-printf("Estoy escrbiendo un " + bird)
+val bird = "tweet"
+printf(s"Estoy escribiendo un ", $bird)
 
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
 //   secuencia "Luke"
+val st = "Hola Luke yo soy tu padre!"
+st slice  (5,9) //Extrae posicion 5 a 9
+
 //5. Cual es la diferencia en value y una variable en scala?
+// R= value (val) obtiene un dato que es inmutable osea, no puede ser modificado 
+// en cambio variable (var) obtiene un dato que puede cambiar 
+// siempre y cuando sea el mismo tipo.
+
 //6. Dada la tupla ((2,4,5),(1,2,3),(3.1416,23))) regresa el numero 3.1416 
+val my_tup = ((2,4,5),(1,2,3),(3.1416,23))
+my_tup._3
