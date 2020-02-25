96265631222a35c4240000f01dd936fa672be1b1
manuel <jose.orozco@tetijuana.edu.mx>
2020-02-25 14:26:39 -0800

    test

--- evaluacion/e1.scala
+++ evaluacion/e1.scala
@@ -1,9 +1,16 @@
-val arr=((11,2,4),(4,5,6),(10,8,-12))
 
-def diagonalDifference(arr:(Int,Int,Int),(Int,Int,Int),(Int,Int,Int)) : Int = {
+Develop a function called diagonalDifference in a scrip with
+the programming language scala. This must return an integer
+that represents the difference of the absolute diagonal.
+
+
+def diagonalDifference() = {
+  val arr=((11,2,4),(4,5,6),(10,8,-12))
   var diagonal1 = (arr._1._1 + arr._2._2 + arr._3._3 )
   var diagonal2=(arr._1._3 + arr._2._2 + arr._3._1)
   var resultado= diagonal1-diagonal2
   var resl =math.abs(resultado)
   println(resl)
 }
+
+diagonalDifference()
