59fcf1ccfe6ec1e9f8baa3758f22a8dbae1ff331
Lilia Rosales <35781915+liliarsis@users.noreply.github.com>
2020-02-24 18:16:21 -0800

    updated practica2

--- practicas_tareas/practica2.scala
+++ practicas_tareas/practica2.scala
@@ -3,16 +3,16 @@
 var lista: List[String] = List("rojo", "blanco", "negro")
 
 // 2. Añadir 5 elementos mas a "lista" "verde" ,"amarillo", "azul", "naranja", "perla"
-val v = "verde" :: lista
-val am = "amarillo" :: lista
-val az = "azul" :: lista
-val n = "naranja" :: lista
-val p = "perla" :: lista
+val lista2 ="verde"::"amarillo"::"azul"::"naranja"::"perla" ::lista
 // 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
 val pasos = List.range(1, 1000, 5)
 // 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
+val lista=Set(1,3,3,4,6,7,3,7)
 // 6. Crea una mapa mutable llamado nombres que contenga los siguiente
 //     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
+val nombres = Map(("jose", 20), ("Luis", 24), ("Ana", 23),("Susana",27))
 // 6 a . Imprime todas la llaves del mapa
+nombres.keys
 // 7 b . Agrega el siguiente valor al mapa("Miguel", 23)
+nombres + ("Miguel" ->23)
