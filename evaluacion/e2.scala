40aa1f1a7987c51817878d11a462b65d6e5815c1
manuel <jose.orozco@tetijuana.edu.mx>
2020-03-02 21:35:27 -0800

    examen 2

---
+++ evaluacion/e2.scala
@@ -0,0 +1,7 @@
+import org.apache.spark.sql.SparkSession
+
+val spar = SparkSession.builder().getOrCreate()
+
+val df = spark.read.option("header", "true").option("inferSchema","true")csv("Netflix_2011_2016.csv")
+
+df.printSchema()
