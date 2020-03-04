2ba421596eb8de589253fc005d133b27b21d7c84
Lilia Rosales <35781915+liliarsis@users.noreply.github.com>
2020-03-03 22:14:30 -0800

    add comments

--- evaluacion/e1.scala
+++ evaluacion/e1.scala
@@ -4,13 +4,14 @@ the programming language scala. This must return an integer
 that represents the difference of the absolute diagonal.*/
 
 
+//diagonalDifference function
 def diagonalDifference() = {
-  val arr=((11,2,4),(4,5,6),(10,8,-12))
-  var diagonal1 = (arr._1._1 + arr._2._2 + arr._3._3 )
-  var diagonal2=(arr._1._3 + arr._2._2 + arr._3._1)
-  var resultado= diagonal1-diagonal2
-  var resl =math.abs(resultado)
-  println(resl)
+  val arr=((11,2,4),(4,5,6),(10,8,-12))//creates array
+  var diagonal1 = (arr._1._1 + arr._2._2 + arr._3._3 )//creates variable with 1,2,3 position
+  var diagonal2=(arr._1._3 + arr._2._2 + arr._3._1)//creates variable with 3,2,1 position
+  var resultado= diagonal1-diagonal2//substraction
+  var resl =math.abs(resultado)//absolut result
+  println(resl)//print result
 }
 
-diagonalDifference()
+diagonalDifference()//calls function
